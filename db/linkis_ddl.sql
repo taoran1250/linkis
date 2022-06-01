ba400bebae0e91b07ad1328d743705e489af55f9
peacewong <wpeace1212@gmail.com>
2022-06-01 11:05:07 +0800

    update 1.1.3 ddl

--- db/linkis_ddl.sql
+++ db/linkis_ddl.sql
@@ -658,12 +658,12 @@ CREATE TABLE `linkis_cg_manager_service_instance` (
 DROP TABLE IF EXISTS `linkis_cg_manager_linkis_resources`;
 CREATE TABLE `linkis_cg_manager_linkis_resources` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `max_resource` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `min_resource` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `used_resource` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `left_resource` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `expected_resource` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `locked_resource` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `max_resource` varchar(1020) COLLATE utf8_bin DEFAULT NULL,
+  `min_resource` varchar(1020) COLLATE utf8_bin DEFAULT NULL,
+  `used_resource` varchar(1020) COLLATE utf8_bin DEFAULT NULL,
+  `left_resource` varchar(1020) COLLATE utf8_bin DEFAULT NULL,
+  `expected_resource` varchar(1020) COLLATE utf8_bin DEFAULT NULL,
+  `locked_resource` varchar(1020) COLLATE utf8_bin DEFAULT NULL,
   `resourceType` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `ticketId` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -740,27 +740,26 @@ CREATE TABLE `linkis_cg_manager_label_resource` (
   UNIQUE KEY `label_id` (`label_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-DROP TABLE IF EXISTS `linkis_cg_ec_resource_info_record`;
 CREATE TABLE `linkis_cg_ec_resource_info_record` (
-    `id` INT(20) NOT NULL AUTO_INCREMENT,
-    `label_value` VARCHAR(255) NOT NULL COMMENT 'ec labels stringValue',
-    `create_user` VARCHAR(128) NOT NULL COMMENT 'ec create user',
-    `service_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'ec instance info',
-    `ecm_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'ecm instance info ',
-    `ticket_id` VARCHAR(100) NOT NULL COMMENT 'ec ticket id',
-    `log_dir_suffix` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'log path',
-    `request_times` INT(8) COMMENT 'resource request times',
-    `request_resource` VARCHAR(255) COMMENT 'request resource',
-    `used_times` INT(8) COMMENT 'resource used times',
-    `used_resource` VARCHAR(255) COMMENT 'used resource',
-    `release_times` INT(8) COMMENT 'resource released times',
-    `released_resource` VARCHAR(255)  COMMENT 'released resource',
-    `release_time` datetime DEFAULT NULL COMMENT 'released time',
-    `used_time` datetime DEFAULT NULL COMMENT 'used time',
-    `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
-    PRIMARY KEY (`id`),
-    KEY (`ticket_id`),
-    UNIQUE KEY `label_value_ticket_id` (`ticket_id`,`label_value`)
+     `id` INT(20) NOT NULL AUTO_INCREMENT,
+     `label_value` VARCHAR(255) NOT NULL COMMENT 'ec labels stringValue',
+     `create_user` VARCHAR(128) NOT NULL COMMENT 'ec create user',
+     `service_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'ec instance info',
+     `ecm_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'ecm instance info ',
+     `ticket_id` VARCHAR(100) NOT NULL COMMENT 'ec ticket id',
+     `log_dir_suffix` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'log path',
+     `request_times` INT(8) COMMENT 'resource request times',
+     `request_resource` VARCHAR(1020) COMMENT 'request resource',
+     `used_times` INT(8) COMMENT 'resource used times',
+     `used_resource` VARCHAR(1020) COMMENT 'used resource',
+     `release_times` INT(8) COMMENT 'resource released times',
+     `released_resource` VARCHAR(1020)  COMMENT 'released resource',
+     `release_time` datetime DEFAULT NULL COMMENT 'released time',
+     `used_time` datetime DEFAULT NULL COMMENT 'used time',
+     `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
+     PRIMARY KEY (`id`),
+     KEY (`ticket_id`),
+     UNIQUE KEY `label_value_ticket_id` (`ticket_id`,`label_value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 DROP TABLE IF EXISTS `linkis_cg_manager_label_service_instance`;
