7b28f0fb3cb37c015d7e0b80ae20030a89638cfd
peacewong <wpeace1212@gmail.com>
2022-07-29 17:22:20 +0800

    remove unlock

--- linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/main/java/org/apache/linkis/manager/dao/LockManagerMapper.java
+++ linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/main/java/org/apache/linkis/manager/dao/LockManagerMapper.java
@@ -30,8 +30,6 @@ public interface LockManagerMapper {
                     + "values(#{jsonObject}, #{timeOut}, now(), now())")
     void lock(@Param("jsonObject") String jsonObject, @Param("timeOut") Long timeOut);
 
-    @Delete("delete  from linkis_cg_manager_lock where lock_object = #{jsonObject}")
-    void unlock(String jsonObject);
 
     @Delete("delete  from linkis_cg_manager_lock where id = #{id}")
     void unlock(int id);
