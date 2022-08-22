0fd1184fdb86b10ade3de50c14e766402f9e360d
peacewong <wpeace1212@gmail.com>
2022-08-22 17:46:16 +0800

    optimize locker

--- linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/main/java/org/apache/linkis/manager/persistence/impl/DefaultLockManagerPersistence.java
+++ linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/main/java/org/apache/linkis/manager/persistence/impl/DefaultLockManagerPersistence.java
@@ -60,8 +60,14 @@ public class DefaultLockManagerPersistence implements LockManagerPersistence {
 
     private boolean tryLock(PersistenceLock persistenceLock, Long timeOut) {
         try {
-            lockManagerMapper.lock(persistenceLock.getLockObject(), timeOut);
-            return true;
+            List<PersistenceLock> lockers =
+                    lockManagerMapper.getLockersByLockObject(persistenceLock.getLockObject());
+            if (lockers == null || lockers.isEmpty()) {
+                lockManagerMapper.lock(persistenceLock.getLockObject(), timeOut);
+                return true;
+            } else {
+                return false;
+            }
         } catch (DataAccessException e) {
             logger.warn("Failed to obtain lock:" + persistenceLock.getLockObject());
             return false;
