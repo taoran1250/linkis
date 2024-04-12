fe68b030734cf029a4efd35147bcdc7902498059
peacewong <peacewong@apache.org>
2024-04-12 18:49:07 +0800

    support

--- linkis-computation-governance/linkis-jdbc-driver/src/main/scala/org/apache/linkis/ujes/jdbc/UJESClientFactory.scala
+++ linkis-computation-governance/linkis-jdbc-driver/src/main/scala/org/apache/linkis/ujes/jdbc/UJESClientFactory.scala
@@ -89,6 +89,18 @@ object UJESClientFactory extends Logging {
       }
     }
     if (!versioned) clientConfigBuilder.setDWSVersion("v" + DEFAULT_VERSION)
+    val sslEnabled =
+      if (
+          props
+            .containsKey(USE_SSL) && "false".equalsIgnoreCase(props.getProperty(USE_SSL))
+      ) {
+        false
+      } else {
+        true
+      }
+    if (sslEnabled) {
+      clientConfigBuilder.setSSL(sslEnabled)
+    }
     UJESClient(clientConfigBuilder.build())
   }
 
