ba57808e0d6c6293fcc4fe354d1780bc6ff1b84b
“v_kkhuang” <420895376@qq.com>
2024-07-08 11:39:04 +0800

    Code optimization

--- linkis-commons/linkis-common/src/main/scala/org/apache/linkis/common/utils/VariableUtils.scala
+++ linkis-commons/linkis-common/src/main/scala/org/apache/linkis/common/utils/VariableUtils.scala
@@ -276,6 +276,9 @@ object VariableUtils extends Logging {
     nameAndType("run_current_mon_now_std") = MonType(
       new CustomMonType(run_roday_mon.toString, true, false)
     )
+    // calculate run_mon_now base on run_today
+    nameAndType("run_mon_now") = MonType(new CustomMonType(run_roday_mon.toString, false, false))
+    nameAndType("run_mon_now_std") = MonType(new CustomMonType(run_roday_mon.toString, true, false))
 
     // calculate run_last_mon base on run_today
     nameAndType("run_today_hour") = HourType(
